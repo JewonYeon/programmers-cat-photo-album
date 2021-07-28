d18bd2152c47202a07fba279ece4e27d8e4e612d
JewonYeon <yjw9852@gmail.com>
2021-07-29 01:54:59 +0900

    feat : Directory Click Event

--- src/App.js
+++ src/App.js
@@ -13,15 +13,25 @@ export class App {
 
     this.nodes = new Nodes({
       $app,
-      initialState: { isRoot: this.state.isRoot, nodes: this.state.nodes },
+      initialState: [],
       // 함수를 파라미터로 던지고, Nodes 내에서 click 발생 시 이 함수를 호출함
       // 이러면 Nodes 내에선 click 후 어떤 로직이 일어날지 알아야 할 필요가 없음
-      onClick: (node) => {
-        if (node.type === "DIRECTORY") {
-          // DIRECTORY인 경우 처리
-          // 여기에서 Breadcrumb 관련 처리를 하게 되면, Nodes에서는 Breadcrumb를 몰라도 됨
-        } else if (node.type === "FILE") {
-          // FILE인 경우 처리
+      onClick: async (node) => {
+        try {
+          if (node.type === "DIRECTORY") {
+            // DIRECTORY인 경우 처리
+            // 여기에서 Breadcrumb 관련 처리를 하게 되면, Nodes에서는 Breadcrumb를 몰라도 됨
+            const nextNodes = await api.directory(node.id);
+            this.setState({
+              ...this.state,
+              depth: [...this.state.depth, node],
+              nodes: nextNodes,
+            });
+          } else if (node.type === "FILE") {
+            // FILE인 경우 처리
+          }
+        } catch (e) {
+          // 에러 처리
         }
       },
     });
