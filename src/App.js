776c82296f84ea27e8883a5e5407a3c736adbec0
JewonYeon <yjw9852@gmail.com>
2021-07-29 00:21:27 +0900

    feat : Connect - App, Nodes

--- src/App.js
+++ src/App.js
@@ -3,5 +3,26 @@ import { ImageView } from "./components/ImageView";
 import { Nodes } from "./components/Nodes";
 
 export class App {
-  constructor() {}
+  constructor($app) {
+    this.state = {
+      isRoot: true,
+      nodes: [],
+      depth: [],
+    };
+
+    this.nodes = new Nodes({
+      $app,
+      initialState: { isRoot: this.state.isRoot, nodes: this.state.nodes },
+      // 함수를 파라미터로 던지고, Nodes 내에서 click 발생 시 이 함수를 호출함
+      // 이러면 Nodes 내에선 click 후 어떤 로직이 일어날지 알아야 할 필요가 없음
+      onClick: (node) => {
+        if (node.type === "DIRECTORY") {
+          // DIRECTORY인 경우 처리
+          // 여기에서 Breadcrumb 관련 처리를 하게 되면, Nodes에서는 Breadcrumb를 몰라도 됨
+        } else if (node.type === "FILE") {
+          // FILE인 경우 처리
+        }
+      },
+    });
+  }
 }
