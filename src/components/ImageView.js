ce0c82c87c11667bf9f53ae1c3b1020031c00de9
JewonYeon <yjw9852@gmail.com>
2021-07-29 02:04:00 +0900

    feat : Rendering - ImageView Component

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,3 +1,30 @@
+// 파일을 클릭한 경우 Modal을 하나 띄우고 해당 Modal에서 파일의 이미지를 렌더링한다.
+
+const IMAGE_PATH_PREFIX =
+  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
+
 export class ImageView {
-  constructor() {}
+  constructor({ $app, initialState }) {
+    // img url
+    this.state = initialState;
+    this.$target = document.createElement("div");
+    this.$target.className = "ImageViewer";
+
+    $app.appendChild(this.$target);
+
+    this.render();
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = `<div class="content"><img src=${IMAGE_PATH_PREFIX}${
+      this.state || ""
+    }></div>`;
+
+    this.$target.style.display = this.state ? "block" : "none";
+  }
 }
